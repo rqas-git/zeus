5d45a62872ddfbdc6fe655a666cb2134796b6a48
rqas-git <rq.sinha+git@gmail.com>
2026-05-04 16:10:10 -0700

    Use zeus-checks as the single local test gate

--- zeus-swift/Package.swift
+++ zeus-swift/Package.swift
@@ -1,12 +1,6 @@
 // swift-tools-version: 6.2
 
 import PackageDescription
-import Foundation
-
-let developerDir = ProcessInfo.processInfo.environment["DEVELOPER_DIR"]
-    ?? "/Library/Developer/CommandLineTools"
-let developerFrameworks = "\(developerDir)/Library/Developer/Frameworks"
-let developerLibraries = "\(developerDir)/Library/Developer/usr/lib"
 
 let package = Package(
     name: "ZeusSwift",
@@ -29,22 +23,6 @@ let package = Package(
             dependencies: ["ZeusCore"],
             swiftSettings: [
                 .swiftLanguageMode(.v5)
-            ]
-        ),
-        .testTarget(
-            name: "ZeusTests",
-            dependencies: ["ZeusCore"],
-            swiftSettings: [
-                .unsafeFlags(["-F", developerFrameworks])
-            ],
-            linkerSettings: [
-                .unsafeFlags([
-                    "-F", developerFrameworks,
-                    "-Xlinker", "-rpath",
-                    "-Xlinker", developerFrameworks,
-                    "-Xlinker", "-rpath",
-                    "-Xlinker", developerLibraries
-                ])
             ]
         ),
         .executableTarget(
